d8d8c0a3914c53a7b413f31956ce9e3a94ed21f8
Junha Yang <junhayang1@gmail.com>
2022-07-14 03:34:22 +0900

    Fix various typos in comments

--- contract-common/src/lib.rs
+++ contract-common/src/lib.rs
@@ -1,6 +1,6 @@
 //! A set of utilities, types, and constants to be used in the contract code, for all the colony chains based on WASM.
 //!
-//! Message-delivery related types (`LightClientUpdageMessage`, `FungibleTokenTransferMessage`, `NonFungibleTokenTransferMessage`, `CustomMessage`)
+//! Message-delivery related types (`LightClientUpdateMessage`, `FungibleTokenTransferMessage`, `NonFungibleTokenTransferMessage`, `CustomMessage`)
 //! are en/decoded between the contract-relayer communication;
 //! you should use the provided (in this crate) serialization method to decode the message given by the transaction,
 //! as the matching serialization method will be used in the relayer side too, which depends on this crate as well.
