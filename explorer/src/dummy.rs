a33be03928dcd679102b93603f20f8287a984408
Junha Yang <junhayang1@gmail.com>
2022-07-16 15:11:04 +0900

    Make ColonyChain as HTTP API using serde-tc

---
+++ explorer/src/dummy.rs
@@ -0,0 +1,100 @@
+use async_trait::async_trait;
+use pbc_colony_common::*;
+use pbc_contract_common::*;
+use pbc_node::PbcApi;
+use rust_decimal::prelude::*;
+use rust_decimal_macros::dec;
+use std::collections::HashMap;
+
+/// A dummy implementation for `trait ColonyChain`.
+///
+/// This is usually for testing the webpage,
+/// as this trait will be directly served as the API of the explorer server.
+pub struct Dummy {}
+
+#[async_trait]
+impl PbcApi for Dummy {}
+
+#[async_trait]
+impl ColonyChain for Dummy {
+    async fn get_chain_name(&self) -> String {
+        "dummy".to_owned()
+    }
+
+    async fn check_connection(&self) -> Result<(), Error> {
+        Ok(())
+    }
+
+    async fn get_contract_list(&self) -> Result<Vec<ContractInfo>, Error> {
+        Ok(vec![
+            ContractInfo {
+                address: "0xabcd".to_owned(),
+                contract_type: ContractType::LightClient,
+                sequence: 0,
+            },
+            ContractInfo {
+                address: "0x1234".to_owned(),
+                contract_type: ContractType::Treasury,
+                sequence: 0,
+            },
+        ])
+    }
+
+    async fn get_relayer_account_info(&self) -> Result<(String, Decimal), Error> {
+        Ok(("0x12341234".to_owned(), dec!(12.34)))
+    }
+
+    async fn get_light_client_header(&self) -> Result<Header, Error> {
+        Ok("Hmm".to_owned())
+    }
+
+    async fn get_treasury_fungible_token_balance(&self) -> Result<HashMap<String, Decimal>, Error> {
+        Ok(vec![
+            ("Bitcoin".to_owned(), dec!(123.45)),
+            ("Ether".to_owned(), dec!(444.44)),
+        ]
+        .into_iter()
+        .collect())
+    }
+
+    async fn get_treasury_non_fungible_token_balance(
+        &self,
+    ) -> Result<Vec<(String, String)>, Error> {
+        Ok(vec![
+            ("BAYC".to_owned(), "1".to_owned()),
+            ("Sandbox Land".to_owned(), "2".to_owned()),
+        ])
+    }
+
+    async fn update_light_client(&self, _message: LightClientUpdateMessage) -> Result<(), Error> {
+        Ok(())
+    }
+
+    async fn transfer_treasury_fungible_token(
+        &self,
+        _message: FungibleTokenTransferMessage,
+        _block_height: u64,
+        _proof: MerkleProof,
+    ) -> Result<(), Error> {
+        Ok(())
+    }
+
+    async fn transfer_treasury_non_fungible_token(
+        &self,
+        _message: NonFungibleTokenTransferMessage,
+        _block_height: u64,
+        _proof: MerkleProof,
+    ) -> Result<(), Error> {
+        Ok(())
+    }
+
+    async fn deliver_custom_order(
+        &self,
+        _contract_name: &str,
+        _message: CustomMessage,
+        _block_height: u64,
+        _proof: MerkleProof,
+    ) -> Result<(), Error> {
+        Ok(())
+    }
+}
--- explorer/src/lib.rs
+++ explorer/src/lib.rs
@@ -1,10 +1,41 @@
+mod dummy;
+
 use pbc_colony_common::*;
 use std::collections::HashMap;
+use std::sync::Arc;
 
+/// Runs a web server that provides useful information about the colony chains and PBC.
 pub async fn run(
-    _colony_chains: HashMap<String, Box<dyn ColonyChain>>,
+    port: u16,
+    mut colony_chains: HashMap<String, Arc<dyn ColonyChain>>,
     _pbc_api: Box<dyn pbc_node::PbcApi>,
 ) {
-    // Runs a web server that provides useful information about the colony chains and PBC.
-    unimplemented!()
+    colony_chains.insert("dummy".to_owned(), Arc::new(dummy::Dummy {}));
+    let colony_chains: HashMap<String, Arc<dyn serde_tc::http::HttpInterface>> = colony_chains
+        .into_iter()
+        .map(|(name, chain)| (name, serde_tc::http::create_http_object(chain)))
+        .collect();
+    serde_tc::http::run_server(port, colony_chains).await;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn check_connection() {
+        tokio::spawn(run(12000, HashMap::new(), Box::new(dummy::Dummy {})));
+
+        let client = reqwest::Client::new();
+        let response = client
+            .post("http://localhost:12000/dummy")
+            .header("content-type", "application/json")
+            .body(r#"{"method": "get_chain_name", "params": {}}"#)
+            .send()
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), reqwest::StatusCode::OK);
+        assert_eq!(response.json::<String>().await.unwrap(), "dummy");
+    }
 }
