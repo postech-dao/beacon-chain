ad9990d2db11fbbe4b433c31aca88a2bf71cf210
Junha Yang <junhayang1@gmail.com>
2022-07-20 19:04:11 +0900

    Setup Dockerfile

---
+++ explorer/src/main.rs
@@ -0,0 +1,13 @@
+use pbc_explorer::*;
+use std::collections::HashMap;
+
+#[tokio::main]
+async fn main() {
+    let port = std::env::args()
+        .nth(1)
+        .unwrap_or_else(|| "80".to_owned())
+        .parse::<u16>()
+        .unwrap();
+    println!("RUN ON PORT {}", port);
+    run(port, HashMap::new(), Box::new(dummy::Dummy {})).await;
+}
